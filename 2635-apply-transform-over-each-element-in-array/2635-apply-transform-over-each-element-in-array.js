f0583d3cbc316d5bd65dc5536e492c96e2c6027b
Agnes Koinange <100143604+Agneskoinange@users.noreply.github.com>
2023-12-12 23:14:35 +0300

    Time: 51 ms (57.90%), Space: 41.8 MB (64.71%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    for(i in arr)
+        arr[i]=fn(arr[i], Number(i));
+            return arr
+};
